d8cd808a796c4335a535c90cf1913f914b61b302
tushargaur720 <115008385+tushargaur720@users.noreply.github.com>
2023-01-09 19:43:13 +0530

    Create operator program 1.c

---
+++ operator program 1.c
@@ -0,0 +1,11 @@
+// A C PROGRAM TO MULTIPLY A NUMBER BY 15 WITHOUT ARITHMATIC OPERATOR
+#include<stdio.h>
+int main()
+{int a,b,c;
+scanf("%d",&a);
+b=4;
+c=(a<<b)-a;
+printf("%d",c);
+return 0;
+
+}
